b4954045ff099e35994ee9442299ccc85464cb41
Matt Agosto <matthewragosto@gmail.com>
2022-03-30 22:59:59 -0400

    Outputting weather measurements

---
+++ ObserverPattern/Displays/CurrentConditionsDisplay.cs
@@ -0,0 +1,34 @@
+﻿using ObserverPattern.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern.Displays
+{
+    public class CurrentConditionsDisplay : IObserver, IDisplayElement
+    {
+        private float temperature;
+        private float humidity;
+        private ISubject weatherData;
+
+        public CurrentConditionsDisplay(ISubject weatherData)
+        {
+            this.weatherData = weatherData;
+            weatherData.registerObserver(this);
+        }
+
+        public void update(float temperature, float humidity, float pressure)
+        {
+            this.temperature = temperature;
+            this.humidity = humidity;
+            display();
+        }
+
+        public void display()
+        {
+            Console.WriteLine($"Current conditions {temperature}F degrees and {humidity}% humidity"); ;
+        }
+    }
+}
--- ObserverPattern/Program.cs
+++ ObserverPattern/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ObserverPattern;
+using ObserverPattern.Interfaces;
+using ObserverPattern.Displays;
+
+WeatherData weatherData = new WeatherData();
+
+CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
+//StatisticsDisplay
+//ForecastDisplay
+
+weatherData.setMeasurements(80, 65, 30.4f);
+weatherData.setMeasurements(82, 70, 29.2f);
+weatherData.setMeasurements(78, 90, 29.2f);
